bb5de3f2fb87f41486613d24b969081869258b48
NewImproved <132500348+NewImproved@users.noreply.github.com>
2025-11-27 14:44:41 +0100

    Refactor options flow initialization
    Removed assignment of config_entry in the constructor.

--- custom_components/aduro /config_flow.py
+++ custom_components/aduro /config_flow.py
@@ -113,7 +113,6 @@ class AduroOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
